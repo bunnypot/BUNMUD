166eac0c2dfa359658d06e962143cb2fd4fb0eb0
bunnypot <ribbonkinbaku@gmail.com>
2017-09-15 13:07:58 +1000

    Added a non-persistent redis storage call to test herokuredis

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+//DISCORD
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const discord_client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+discord_client.login(process.env.DISCORD_TOKEN);
+
+//REDIS
+const Redis = require("redis"),
+const redis_client = redis.createClient(process.env.REDIS_URL);
+
+redis_client.set("pings", 0);
+
+
+//EVENTS
+discord_client.on('message', msg => {
+	if (msg.content === 'ping') {
+		
+		redis_client.get("pings", function (err, reply) {
+			redis_client.set("pings", reply+1);
+			msg.reply('Pong! ping count is '+reply+1);
+		});
+	}	
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login(process.env.DISCORD_TOKEN);
