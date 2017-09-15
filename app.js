a0e41387dbc4bebff4455927c1620dc0c867826d
bunnypot <ribbonkinbaku@gmail.com>
2017-09-15 13:14:01 +1000

    added extra debugging

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ discord_client.login(process.env.DISCORD_TOKEN);
 const Redis = require("redis"),
 const redis_client = redis.createClient(process.env.REDIS_URL);
 
-redis_client.set("pings", 0);
-
+redis_client.set("pings", 0, redis.print);
 
 //EVENTS
 discord_client.on('message', msg => {
 	if (msg.content === 'ping') {
 		
 		redis_client.get("pings", function (err, reply) {
-			redis_client.set("pings", reply+1);
+			console.log(reply)
+			redis_client.set("pings", reply+1, redis.print);
 			msg.reply('Pong! ping count is '+reply+1);
 		});
 	}	
