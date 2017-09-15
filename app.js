faa50c0634f74af319b6054f66de8872f142b223
bunnypot <ribbonkinbaku@gmail.com>
2017-09-15 13:49:59 +1000

    Revert "added extra debugging"
    This reverts commit 7fc40715141b29ed4c97a7b32ff2cef61a4c300d.

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ discord_client.login(process.env.DISCORD_TOKEN);
 
 //REDIS
 const Redis = require("redis"),
-const redis_client = Redis.createClient(process.env.REDIS_URL);
+const redis_client = redis.createClient(process.env.REDIS_URL);
 
-redis_client.set("pings", 0, Redis.print);
+redis_client.set("pings", 0);
+
 
 //EVENTS
 discord_client.on('message', msg => {
 	if (msg.content === 'ping') {
+		
 		redis_client.get("pings", function (err, reply) {
-			console.log(reply)
-			redis_client.set("pings", reply+1, Redis.print);
+			redis_client.set("pings", reply+1);
 			msg.reply('Pong! ping count is '+reply+1);
 		});
 	}	
