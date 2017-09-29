532f7b76bf436c6b16f6c7d20942245a459d5505
bunnypot <ribbonkinbaku@gmail.com>
2017-09-29 11:13:09 +1000

    Added dice evaluator, added a util function for parsing messages and returning objects.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//LIFECYCLE --------------------------------------
+
 //DISCORD
 const Discord = require("discord.js");
 global.discord_client = new Discord.Client();
@@ -8,7 +10,13 @@ const Redis = require("redis");
 global.redis_client = Redis.createClient(process.env.REDIS_URL);
 global.redis_client.set("pings", 0, Redis.print);
 
-//EVENTS
+//ROLLS
+global.DiceExpression = require('dice-expression-evaluator');
+
+//CHARACTER
+//const Character = require("./src/Character/Character.js");
+
+//EVENTS --------------------------------------
 global.discord_client.on("ready", () => {
     const channel = global.discord_client.channels.get("358044545549598721");
     if (!channel) return;
@@ -16,7 +24,7 @@ global.discord_client.on("ready", () => {
 });
 
 global.discord_client.on('message', msg => {
-	if(msg.author.id == "297229074777112576") { return; } 
+	msg.author.id == "297229074777112576"
 
 	if (msg.content === "ping") {
 		redis_client.get("pings", function (err, reply) {
@@ -24,8 +32,39 @@ global.discord_client.on('message', msg => {
 			msg.reply("Pong! ping count is "+(parseInt(reply)+1));
 		});
 	}
+	commandObject = commandParse(msg.content);
+	switch(commandObject.command) {
+		case "d": 
+			//diceroll
+			msg.reply("`Result :"+ new global.DiceExpression(commandObject.args.join(''))()+ "`");
+			break;
+		case "c":
+
+			break;
+	}
+	
 });
 
+//UTIL  --------------------------------------
+function commandParse(text) {
 
-//GRID NET CHARACTER
-const Character = require("./src/Character/Character.js");
+	//command validation
+	if (text == null || text == "" || text.substr(0,1) != ".") { return false; } 
+
+	//will return object of {"command": command (string) "args": [arguments]}
+	//splits by " " and ";"
+	if (text.indexOf(";") == -1) {
+		var t = text.split(" ");
+		if (t.length == 1) {
+			return {"command":t.toString().substr(1)};
+		} else {
+			var c = t.shift();
+			return {"command": c.substr(1), "args": t};
+		}
+	} else {
+        var t = text.split(" ");
+		var c = t.shift();
+		return {"command": c.substr(1), "args": t.join("").split(";")}
+	}
+
+}
